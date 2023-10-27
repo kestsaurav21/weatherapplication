4eaa34833755111e1de383e683d2b8b8cbdd9b01
kestsaurav21 <kestsaurav21@outlook.com>
2023-10-28 00:53:23 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import WeatherApp from './Componensts/WeatherApp/WeatherApp';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
